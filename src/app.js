1e869974efa4cc5620a5c0d2675e4ad1a55406cf
Andromashka <andromashka@ukr.net>
2022-06-19 23:46:08 +0300

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElement=document.querySelector("#temperature");
+  let cityElement=document.querySelector("#city");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind");
+  windElement.innerHTML=Math.round(response.data.wind.speed);
+  humidityElement.innerHTML=response.data.main.humidity;
+  descriptionElement.innerHTML=response.data.weather[0].description;
+  cityElement.innerHTML=response.data.name;
+  temperatureElement.innerHTML=Math.round(response.data.main.temp);
+}
 
+let apiKey = "e703fefde0fb0347c2ba5d655daa1cbe";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Brasov&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
